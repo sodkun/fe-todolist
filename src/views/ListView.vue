b0627a29b13bcbaf9c4d6df9bd222363e06b2a51
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2023-02-23 00:37:39 +0700

    add list view

---
+++ src/views/ListView.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'ListView',
+  data: () => ({
+    list: [
+      {
+        name: 'First list'
+      },
+      {
+        name: 'Second list'
+      }
+    ]
+  })
+}
+</script>
+
+<template>
+  <div>
+    <h1>List</h1>
+
+    <ol>
+      <template v-for="(item, index) in list" :key="index">
+        <li>{{ item.name }}</li>
+      </template>
+    </ol>
+  </div>
+</template>
