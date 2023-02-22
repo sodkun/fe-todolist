6dcbe500058e7c49c42acb5610b7a3e5b0feb5d3
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2023-02-23 00:46:23 +0700

    add input

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,6 +2,9 @@
 export default {
   name: 'ListView',
   data: () => ({
+    input: {
+      name: ''
+    },
     list: [
       {
         name: 'First list'
@@ -10,14 +13,25 @@ export default {
         name: 'Second list'
       }
     ]
-  })
+  }),
+  methods: {
+    addList() {
+      this.list.push({ ...this.input })
+      this.input.name = ''
+    }
+  }
 }
 </script>
 
 <template>
   <div>
     <h1>List</h1>
-
+    <!-- <input
+      v-model="input.name"
+      type="text"
+      @keyup.enter="list.push({ ...input })"
+    /> -->
+    <input v-model="input.name" type="text" @keyup.enter="addList" required />
     <ol>
       <template v-for="(item, index) in list" :key="index">
         <li>{{ item.name }}</li>
